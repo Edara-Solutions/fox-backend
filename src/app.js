2121b853930db2334add628914597beb17a61f19
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2026-05-13 23:07:32 +0300

    chore(app): updating the cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const errorMiddleware = require("./middlewares/error.middleware");
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: env.corsOrigin, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, limit: 300 }));
